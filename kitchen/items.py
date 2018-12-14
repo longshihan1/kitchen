68a5db48d31b29fd770d044e2e5c8d13452e0e18
longhe <LONGHE001@huazhu.com>
2018-12-14 18:22:30 +0800

    添加数据库

--- kitchen/items.py
+++ kitchen/items.py
@@ -9,6 +9,17 @@ import scrapy
 
 
 class KitchenItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    title = scrapy.Field()  # Field()能够接收和传递任何类型的值,类似于字典的形式
+    create_date = scrapy.Field()  # 创建时间
+    topimg=scrapy.Field()#头图
+    url = scrapy.Field()  # 文章路径
+    object_id = scrapy.Field()  # 文章内容的md5的哈希值，能够将长度不定的 url 转换成定长的序列
+    score = scrapy.Field()
+    cookcount = scrapy.Field()
+    author = scrapy.Field()
+    author_img = scrapy.Field()
+    desmessage = scrapy.Field()
+    imgsmessage = scrapy.Field()
+    stepsmessage = scrapy.Field()
+    tip = scrapy.Field()
+    videourl = scrapy.Field()
--- kitchen/spiders/haodou.py
+++ kitchen/spiders/haodou.py
@@ -4,6 +4,8 @@ from urllib import parse
 import scrapy
 from scrapy import Request
 
+from kitchen.items import KitchenItem
+from kitchen.utils.common import gen_md5
 
 
 class HaodouSpider(scrapy.Spider):
@@ -12,32 +14,49 @@ class HaodouSpider(scrapy.Spider):
     start_urls = ['http://www.xiachufang.com/category/51761/']
 
     def parse(self, response):
-        #写法1
+        # 写法1
         lists = response.xpath("//div[@class='recipe recipe-215-horizontal pure-g image-link display-block']")
         for element in lists:
-            detail_url=element.xpath("div/p[@class='name']/a/@href").extract()[0]
-            yield Request(url=parse.urljoin(response.url, detail_url),callback=self.parse_detail)
+            detail_url = element.xpath("div/p[@class='name']/a/@href").extract()[0]
+            yield Request(url=parse.urljoin(response.url, detail_url),
+                          meta={"url": parse.urljoin(response.url, detail_url)},
+                          callback=self.parse_detail)
         # 写法2
-        #url_lists=response.xpath("//div/p[@class='name']/a/@href").extract()
-        #for url in url_lists:
+        # url_lists=response.xpath("//div/p[@class='name']/a/@href").extract()
+        # for url in url_lists:
         #    print(url)
         text = response.xpath("//h1[@class='page-title']/text()").extract()[0]
         print(text)
         pass
 
-
-    def parse_detail(self,response):
-        title=response.xpath("//h1[@class='page-title']/text()").extract()[0]
-        topimg=response.xpath("//div[@class='cover image expandable block-negative-margin']/img/@src").extract()[0]
-        #/html/body/div[4]/div/div/div[1]/div[1]/div/div[2]/div[1]/div[1]/span[1]
-        score=response.xpath("//div[@class='score float-left']/span[@class='number']/text()").extract()[0]
+    def parse_detail(self, response):
+        item = KitchenItem()
+        title = response.xpath("//h1[@class='page-title']/text()").extract()[0]
+        topimg = response.xpath("//div[@class='cover image expandable block-negative-margin']/img/@src").extract()[0]
+        # /html/body/div[4]/div/div/div[1]/div[1]/div/div[2]/div[1]/div[1]/span[1]
+        score = response.xpath("//div[@class='score float-left']/span[@class='number']/text()").extract()[0]
         cookcount = response.xpath("//div[@class='cooked float-left']/span[@class='number']/text()").extract()[0]
         author = response.xpath("//div[@class='author']/a/span/text()").extract()[0]
         author_img = response.xpath("//div[@class='author']/a/img/@src").extract()[0]
-        one_message=response.xpath("//div[@class='desc mt30']/text()").extract()[0]
-        tools_message=response.xpath("//div[@class='ings']/text()").extract()[0]
+        one_message = response.xpath("//div[@class='desc mt30']/text()").extract()[0]
+        tools_message = response.xpath("//div[@class='ings']/text()").extract()[0]
         message = response.xpath("//div[@class='steps']/text()").extract()[0]
         tip = response.xpath("//div[@class='tip']/text()").extract()[0]
-        video_url=response.xpath("//iframe/@src").extract()[0]
+        video_url = response.xpath("//iframe/@src").extract()[0]
         print(title)
-        pass
+        url = response.meta.get("url", "")
+        item["title"] = title
+        item["create_date"] = ""
+        item["topimg"] = topimg
+        item["url"] = url
+        item["object_id"] = gen_md5(url)
+        item["score"] = score
+        item["cookcount"] = cookcount
+        item["author"] = author
+        item["author_img"] = author_img
+        item["desmessage"] = one_message
+        item["imgsmessage"] = tools_message
+        item["stepsmessage"] = message
+        item["tip"] = tip
+        item["videourl"] = video_url
+        yield item
